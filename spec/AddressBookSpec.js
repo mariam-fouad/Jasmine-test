af7359915cef962f59121bae06af24f409058a77
Mariam <mariam.fouad94@gmail.com>
2019-01-06 18:10:49 +0300

    create a new test

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,10 +1,21 @@
 describe ("AddressBook",()=>{
-    it("Should be able to add contact",()=>{
+    it("Should be able to add a contact",()=>{
         const addressBook = new AddressBook ();
         const contact = new Contact ();
 
         addressBook.addContact(contact);
 
         expect(addressBook.getContact(0)).toBe(contact);
+    });
+
+    it("Should be able to delete a contact",()=>{
+        const addressBook = new AddressBook ();
+        const contact = new Contact ();
+
+        addressBook.addContact(contact);
+
+        addressBook.deleteContact(0);
+
+        expect(addressBook.getContact(0)).not.toBeDefined();
     });
 }) ;
