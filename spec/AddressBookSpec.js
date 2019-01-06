2aaba4c0e63a8e9bf29033881727ae764aede666
Mariam <mariam.fouad94@gmail.com>
2019-01-06 18:16:40 +0300

    clean up the code

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,17 +1,19 @@
 describe ("AddressBook",()=>{
-    it("Should be able to add a contact",()=>{
-        const addressBook = new AddressBook ();
-        const contact = new Contact ();
 
+    let addressBook,contact;
+
+    beforeEach(()=>{
+        addressBook = new AddressBook ();
+       contact = new Contact ();
+    });
+
+    it("Should be able to add a contact",()=>{
         addressBook.addContact(contact);
 
         expect(addressBook.getContact(0)).toBe(contact);
     });
 
     it("Should be able to delete a contact",()=>{
-        const addressBook = new AddressBook ();
-        const contact = new Contact ();
-
         addressBook.addContact(contact);
 
         addressBook.deleteContact(0);
